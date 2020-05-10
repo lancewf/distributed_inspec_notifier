55b4bf0b236c5c71e61bf9977031ef514ea9d84f
Lance Finfrock <lfinfrock@chef.io>
2020-05-10 16:55:24 -0700

    Rust Habitat Starter
    https://github.com/lancewf/rust-habitat-example
    
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,3 +4,26 @@ pkg_version="0.1.0"
 pkg_maintainer="Lance Finfrock <lancewf@gmail.com>"
 pkg_license=("Apache-2.0")
 pkg_bin_dirs=(bin)
+
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+
+pkg_build_deps=(
+  core/rust
+  core/gcc
+  core/pkg-config
+  core/make
+)
+
+do_build() {
+  pushd "${PLAN_CONTEXT}/.."
+    cp -r src ${CACHE_PATH}/.
+    cp Cargo.toml ${CACHE_PATH}/.
+  popd
+}
+
+do_install() {
+  cargo install --root "${pkg_prefix}" --path "${CACHE_PATH}" --verbose
+}
