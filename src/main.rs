2c113626982e86cde7940421ef65d86e4669860b
Lance Finfrock <lfinfrock@chef.io>
2020-05-10 18:04:44 -0700

    Adding a config file
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 extern crate actix_web;
 use actix_web::{HttpServer, App, web, HttpRequest, HttpResponse};
 use std::env;
+use std::fs;
+use toml;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    service: Service,
+}
+
+#[derive(Deserialize)]
+struct Service {
+    host: String,
+    port: u16,
+}
 
 // Here is the handler, 
 // we are returning a json response with an ok status 
@@ -11,8 +25,16 @@ fn index(_req: HttpRequest) -> HttpResponse  {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let address = &args[1];
-    println!("listening on {}", address);
+    let config_file = &args[1];
+    println!("config file {}", config_file);
+    let file_contents = fs::read_to_string(config_file).unwrap();
+
+    let config: Config = toml::from_str(file_contents.as_str()).unwrap();
+
+    let address = format!("{}:{}", config.service.host, config.service.port);
+
+    println!("listening on address {}", address);
+
     // We are creating an Application instance and 
     // register the request handler with a route and a resource 
     // that creates a specific path, then the application instance 
